074ef139d792d46494d062babb72d9734dfb2d66
Abdul Javeed <70013393+ajaveed7@users.noreply.github.com>
2022-08-07 14:36:07 +0530

    Add files via upload
    SVM Support Vector machine use for classification, SVR is Support Vector Regressor use for Regression. 
    Marginal Hyper Plane, the line which is linearly  able to separate the data points it is called as LSVM(Linear Support Vector Machine), in non linear SVM data point will be clubbed and we use kernel to increase dimensions. Kernal --> Low Dimensions to High Dimensions
    PCA --> High Dimensions to Low Dimensions.

---
+++ Support Vector machine.py
@@ -0,0 +1,57 @@
+#import libraries
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+dataset = pd.read_csv(r"E:\Data Science Post Class\Machine Learning\Classification\Logistic Classification\2.LOGISTIC REGRESSION CODE\Social_Network_Ads.csv")
+
+X=dataset.iloc[:,[2,3]].values
+y=dataset.iloc[:,-1:].values
+
+from sklearn.model_selection import train_test_split
+X_train,X_test,y_train,y_test = train_test_split(X,y,test_size=0.20,random_state=0)
+
+from sklearn.preprocessing import StandardScaler  # Normalizer
+sc=StandardScaler()
+X_train = sc.fit_transform(X_train)
+X_test = sc.transform(X_test)
+
+#SVM Classification 
+from sklearn.svm import SVC
+SVM_classifier = SVC()
+SVM_classifier.fit(X_train,y_train.ravel())
+
+y_SVM = SVM_classifier.predict(X_test)
+
+from sklearn.metrics import confusion_matrix
+cm = confusion_matrix(y_test,y_SVM)
+cm
+
+from sklearn.metrics import accuracy_score
+ac = accuracy_score(y_test,y_SVM)
+ac
+
+from sklearn.metrics import classification_report
+cr = classification_report(y_test,y_SVM)
+cr
+
+
+bias = SVM_classifier.score(X_train,y_train)
+bias
+
+variance = SVM_classifier.score(X_test,y_test)
+variance
+
+fut_data = pd.read_excel(r"E:\Data Science Post Class\Machine Learning\Classification\future prediction.xlsx")
+fut_X = fut_data.iloc[:,[2,3]].values
+fut_X = sc.fit_transform(fut_X)
+SVM_prediction = SVM_classifier.predict(fut_X)
+
+future_prediction = pd.read_excel(r"E:\Data Science Post Class\Machine Learning\Classification\predicted1.xlsx")
+future_prediction["SVM Prediction"]=KNN_prediction
+
+future_prediction.to_excel("E:\Data Science Post Class\Machine Learning\Classification\predicted1.xlsx",index=False)
+
+
+
+
